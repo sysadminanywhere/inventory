cc376636a64e9b6099b9749c21b4de1e262e9000
sysadminanywhere <info@sysadminanywhere.com>
2025-01-07 11:39:23 +0300

    fix

--- src/main/java/com/sysadminanywhere/inventory/repository/ComputerRepository.java
+++ src/main/java/com/sysadminanywhere/inventory/repository/ComputerRepository.java
@@ -15,7 +15,6 @@ public interface ComputerRepository extends JpaRepository<Computer, Long>, JpaSp
 
     List<Computer> findAllByName(String cn);
 
-    // select c."name", c.dns from computers c inner join installations i on c.id = i.computer_id where i.software_id = 1
     @Query("SELECT new com.sysadminanywhere.inventory.controller.dto.ComputerDto(c.id, c.name, c.dns) FROM Computer c JOIN c.installations i WHERE i.software.id = :softwareId and c.name LIKE :name")
     Page<ComputerDto> getComputersWithSoftware(@Param("softwareId") Long softwareId, @Param("name") String name, Pageable pageable);
 
